6e894f6cef9948a73ded884d4c85ec0fe9b1c6bb
gyoscha <sokolovgeorgy@gmail.com>
2022-01-23 14:47:03 +0300

    Homework 4

--- Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
+++ Зачетное задание/Зачетное задание/DoubleLinkedList/task.py
@@ -1,5 +1,197 @@
-class LinkedList:
-    ...
+from _collections_abc import MutableSequence
+from typing import Any, Iterable, Optional, Iterator
+
+from node import Node, DoubleLinkedNode
+
+
+class LinkedList(MutableSequence):
+    def __init__(self, data: Iterable = None):
+        """Конструктор связного списка"""
+        self.len = 0
+        self.head: Optional[Node] = None
+        self.tail = self.head
+
+        if data is not None:
+            for value in data:
+                self.append(value)
+
+    def append(self, value: Any):
+        """ Добавление элемента в конец связного списка. """
+        append_node = Node(value)
+
+        if self.head is None:
+            self.head = self.tail = append_node
+        else:
+            self.linked_nodes(self.tail, append_node)
+            self.tail = append_node
+
+        self.len += 1
+
+    def step_by_step_on_nodes(self, index: int) -> Node:
+        """ Функция выполняет перемещение по узлам до указанного индекса. И возвращает узел. """
+        if not isinstance(index, int):
+            raise TypeError('Индекс не соответствует типу int.')
+
+        if not 0 <= index < self.len:  # для for
+            raise IndexError('Индекс выходит за рамки связного списка.')
+
+        current_node = self.head
+        for _ in range(index):
+            current_node = current_node.next
+
+        return current_node
+
+    @staticmethod
+    def linked_nodes(left_node: Node, right_node: Optional[Node] = None) -> None:
+        """
+        Функция, которая связывает между собой два узла.
+
+        :param left_node: Левый или предыдущий узел
+        :param right_node: Правый или следующий узел
+        """
+        left_node.next = right_node
+
+    def to_list(self) -> list:
+        return [linked_list_value for linked_list_value in self]
+
+    def __getitem__(self, index: int) -> Any:
+        """ Метод возвращает значение узла по указанному индексу. """
+        node = self.step_by_step_on_nodes(index)
+        return node.value
+
+    def __setitem__(self, index: int, value: Any) -> None:
+        """ Метод устанавливает значение узла по указанному индексу. """
+        node = self.step_by_step_on_nodes(index)
+        node.value = value
+
+    def __delitem__(self, index: int) -> None:
+        """
+        Метод удаляет узел по указанному индексу.
+        :param index: Индекс удаляемого узла.
+        :return: None
+        """
+        if not isinstance(index, int):
+            raise TypeError('Индекс не соответствует типу int.')
+
+        if not 0 <= index < self.len:  # для for
+            raise IndexError('Индекс выходит за рамки связного списка.')
+
+        if index == 0:
+            self.head = self.head.next
+        elif index == self.len - 1:
+            tail = self.step_by_step_on_nodes(index - 1)
+            tail.next = None
+        else:
+            prev_node = self.step_by_step_on_nodes(index - 1)
+            del_node = prev_node.next
+            next_node = del_node.next
+
+            self.linked_nodes(prev_node, next_node)
+
+        self.len -= 1
+
+    def __len__(self) -> int:
+        """
+        Метод определения длины последовательности.
+        :return: Значение длины
+        """
+        return self.len
+
+    def insert(self, index: int, value: Any) -> None:
+        """
+        Добавление в LinkedList узла по опреденному идексу.
+        :param index: Индекс, по которому хотим вставить элемент.
+        :param value: Значение, которое хотим вставить.
+        :return: None
+        """
+        if not isinstance(index, int):
+            raise TypeError('Индекс не соответствует типу int.')
+
+        insert_node = Node(value)
+        if index == 0:
+            insert_node.next = self.head
+            self.head = insert_node
+            self.len += 1
+        elif index >= self.len - 1:
+            self.append(value)
+            self.tail = insert_node
+        else:
+            prev_node = self.step_by_step_on_nodes(index - 1)
+            next_node = prev_node.next
+            self.linked_nodes(prev_node, insert_node)
+            self.linked_nodes(insert_node, next_node)
+
+            self.len += 1
+
+    def __str__(self):
+        return f"{self.to_list()}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.to_list()})"
+
+    def nodes_iterator(self) -> Iterator[Node]:
+        """
+        Итератор по узлам LinkedList.
+        :return: Итератор.
+        """
+        current_node = self.head
+        for _ in range(self.len):
+            yield current_node
+            current_node = current_node.next
+
+    def __iter__(self) -> Iterator[Any]:
+        """
+        Переопределяем метод iter для LinkedList.
+        :return: Итератор.
+        """
+        for node in self.nodes_iterator():
+            yield node.value
+
+    def __contains__(self, item: Any) -> bool:
+        """
+        Метод проверяет вхождение значения item в LinkedList.
+        :param item: Значение, вхождение котрого хотим проверить.
+        :return: True/False
+        """
+        for node in self.nodes_iterator():
+            if node.value == item:
+                return True
+
+        return False
+
+    def __reversed__(self) -> None:
+        """
+        Метод для реализации обратного итерирования по объекту.
+        """
+        prev_node = None
+        current_node = self.head
+        while current_node is not None:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        self.head = self.tail
+
+    def count(self, value: Any) -> int:
+        pass
+
+    # def pop(self, index: int = ...) -> :
+    #     pass
+    #
+    # def extend(self, values: Iterable[_T]) -> None:
+    #     pass
+
+    def remove(self, value: Any) -> None:
+        pass
+
+    def index(self, value: Any, start: int = ..., stop: int = ...) -> int:
+        """ Метод ищет элемент в списке и возвращает его индекс. """
+        if value not in self.to_list():
+            raise ValueError('Введенное значение отсутсвтует')
+
+        for index, val in enumerate(self.to_list()):
+            if val == value:
+                return index
 
 
 class DoubleLinkedList(LinkedList):
@@ -7,4 +199,7 @@ class DoubleLinkedList(LinkedList):
 
 
 if __name__ == "__main__":
-    ...
+    a = LinkedList([1, 2, 3, 4, 5])
+    print(a)
+    print(a.index(5))
+
--- Паттерны/Домашнее задание/task2_LinkedList_Mutable_sequence/main.py
+++ Паттерны/Домашнее задание/task2_LinkedList_Mutable_sequence/main.py
@@ -1,4 +1,22 @@
-# TODO Реализовать LinkedList как MutableSequence
+from _collections_abc import MutableSequence
+from typing import Any
+
+
+class LinkedList(MutableSequence):
+    def __getitem__(self, item: int) -> Any:
+        pass
+
+    def __setitem__(self, key: int, value: Any) -> None:
+        pass
+
+    def __delitem__(self, key: int) -> None:
+        pass
+
+    def __len__(self) -> int:
+        pass
+
+    def insert(self, index: int, value: Any) -> None:
+        pass
 
 
 if __name__ == "__main__":
